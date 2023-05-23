f325faeb7247e56821305611e7bb06b10d581d06
KNUAI <86586602+KNUAI@users.noreply.github.com>
2023-05-24 08:52:26 +0900

    Update app_qa_wiki.py
    modify sidebar

--- app_qa_wiki.py
+++ app_qa_wiki.py
@@ -1,20 +1,12 @@
 import os
-from llama_index import GPTVectorStoreIndex
+from langchain import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
 from llama_index import download_loader
 import streamlit as st
 
-import argparse
-
-def arg_parser():
-    # Arguement parsing..
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--apikey", type=str, required=True, help="If you don't know key value, Just ask jskim")
-    args = parser.parse_args()
-    os.environ['OPENAI_API_KEY'] = args.apikey
-    return args
-
 @st.cache_resource
-def _index_from_wiki(keyword):
+def _index_from_wiki(keyword, api_key):
 # create a wikipedia download loader object
     WikipediaReader = download_loader("WikipediaReader")
 
@@ -23,7 +15,7 @@ def _index_from_wiki(keyword):
     documents = loader.load_data(pages=[keyword])
 
     # construct the index with the Wikipedia document
-    index_wiki = GPTVectorStoreIndex.from_documents(documents)
+    index_wiki = VectorstoreIndexCreator(embedding=OpenAIEmbeddings(open_api_key=api_key)).from_documents(documents)
 
     return index_wiki
 
@@ -33,28 +25,51 @@ def main():
     st.markdown(
         "in progress"
         )
-    st.sidebar.markdown(
-        "still in progress"
+    with st.sidebar:
+        embeddeing_model = st.selectbox(
+            label='Embedding Model',
+            options=['text-embedding-ada-002']
         )
 
-    # Load file setting
-    keyword = st.selectbox("**Please select area you're looking for.**", 
-                                   ('Seoul', 'Incheon', 'Daegu'))
-    if keyword:
-        index_wiki=_index_from_wiki(keyword)
-        query_engine = index_wiki.as_query_engine()
-
-        # Get question And Answer. Run !
-        user_question = st.text_input(
-            "**Please insert your question.**",
-            placeholder=f'Ask me anything from {keyword}'
+        chat_model = st.selectbox(
+            label='Chat Model',
+            options=["gpt-3.5-turbo"]
         )
-            
-        if user_question:
-            with st.spinner('Running to answer your question ..'):
-                result = query_engine.query(user_question).response
-                st.info(result, icon='🤖')
+
+        temperature = st.slider(
+            "Temperature",
+            0.0, 1.0, 0.7,
+        )
+
+        st.markdown(
+            """
+            **Blog post:** \n
+            [*Blog title*](URL)
+
+            **Code:** \n
+            [*Github*](https://github.com/jskim0406/SimpleRec_w_langchain.git)
+            """
+        )
+    api_key = st.text_input(
+        "Enter Open AI Key.", placeholder = "sk-...", type="password")
+    os.environ['OPENAI_API_KEY'] = api_key
+    if api_key:
+        # Load file setting
+        keyword = st.selectbox("**Please select area you're looking for.**", 
+                                    ('Seoul', 'Incheon', 'Daegu'))
+        if keyword:
+            index_wiki=_index_from_wiki(keyword, api_key)
+
+            # Get question And Answer. Run !
+            user_question = st.text_input(
+                "**Please insert your question.**",
+                placeholder=f'Ask me anything from {keyword}'
+            )
+                
+            if user_question:
+                with st.spinner('Running to answer your question ..'):
+                    result = index_wiki.query(user_question, llm=OpenAI(temperature=temperature, openai_api_key=api_key, model_name=chat_model))
+                    st.info(result, icon='🤖')
 
 if __name__=='__main__':
-    arg_parser()
     main()
